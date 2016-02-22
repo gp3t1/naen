08c4e92f6ad933f5f7f5de1e2b3d1a19ecd971dd
gp3t1 <gp3t1@openmailbox.org>
2016-02-22 17:29:07 +0100

    add build scripts for nginx image

---
+++ build_nginx.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SCRIPT_PATH=$(dirname "$(realpath -s "$0")")
+cd "$SCRIPT_PATH"
+
+DIR=./nginx
+REPO=gp3t1/nginx
+TAG="$( cat ./.version )-$( egrep '^FROM.*' $DIR/Dockerfile | awk -F ':' '{ print $2; }' )"
+
+if docker build -q "$@" -t "$REPO:$TAG" "$DIR"; then
+	echo "built $REPO:$TAG"
+	exit 0
+fi
+exit 1
