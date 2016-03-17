9d53d06a6e37f6c1db07dceac68162d5d7771838
gp3t1 <gp3t1@openmailbox.org>
2016-03-17 05:55:09 +0100

    prints

--- .dev/release.sh
+++ .dev/release.sh
@@ -24,12 +24,22 @@ while [[ -z $message ]]; do
 	read message
 done
 
-
+echo -e "\nUpdate images version in $DEV_COMPOSE..."
 sed -i "s|image: gp3t1/commafeed:.*$|image: gp3t1/commafeed:release-$version|
 				s|image: gp3t1/postgres-hb:.*$|image: gp3t1/postgres-hb:release-$version|
 				s|image: gp3t1/nginx:.*$|image: gp3t1/nginx:release-$version|" "$DEV_COMPOSE"
-git add "$DEV_COMPOSE"
-git commit -m "update images names in docker-compose"
 
+echo "Commit changes in $DEV_COMPOSE..."
+git add "$DEV_COMPOSE"
+git commit -m "update images version in docker-compose"
+
+echo "Pushing all branches to origin and only master to gitub..."
 git push && git push github master
+
+echo "Tagging actual state with $version and push tag to origin and github"
 git tag -a "$version" -m "$message" && git push origin "$version" && git push github "$version"
+
+echo "Revert images version in $DEV_COMPOSE to latest (workdir only, not committed)..."
+sed -i "s|image: gp3t1/commafeed:.*$|image: gp3t1/commafeed:latest|
+				s|image: gp3t1/postgres-hb:.*$|image: gp3t1/postgres-hb:latest|
+				s|image: gp3t1/nginx:.*$|image: gp3t1/nginx:latest|" "$DEV_COMPOSE"
